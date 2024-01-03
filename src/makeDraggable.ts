74e8f44d4a7654dd379740e2f3bb9b637511781b
Gnlow <gnlow@protonmail.com>
2024-01-03 22:33:44 +0900

    snap to start position

--- src/makeDraggable.ts
+++ src/makeDraggable.ts
@@ -5,13 +5,30 @@ export const makeDraggable = (target: SVGElem) => {
     const on = target.dom.addEventListener
     const off = target.dom.removeEventListener
     on("pointerdown", e => {
+        const start = {
+            x: target.x,
+            y: target.y,
+        }
         const offset = {
             x: e.clientX - target.x,
             y: e.clientY - target.y,
         }
         const onMove = (e: PointerEvent) => {
-            target.x = e.clientX - offset.x
-            target.y = e.clientY - offset.y
+            const now = {
+                x: e.clientX - offset.x,
+                y: e.clientY - offset.y,
+            }
+
+            const dx = now.x - start.x
+            const dy = now.y - start.y
+            const d = Math.sqrt(dx**2 + dy**2)
+            if (d > 20) {
+                target.x = now.x
+                target.y = now.y
+            } else {
+                target.x = start.x
+                target.y = start.y
+            }
         }
         on("pointermove", onMove)
         on("pointerup", e => {
